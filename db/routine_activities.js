49df3cce1965b577f64a1682b90696f29dd3daad
Robert Allred <rallred84@gmail.com>
2023-06-04 21:11:41 -0500

    Passing all db tests

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -20,15 +20,82 @@ async function addActivityToRoutine({
   return newRoutineActivity;
 }
 
-async function getRoutineActivityById(id) {}
+async function getRoutineActivityById(id) {
+  const {
+    rows: [routineActivity],
+  } = await client.query(
+    `
+    SELECT * FROM routine_activities
+    WHERE id = $1;
+    `,
+    [id]
+  );
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+  return routineActivity;
+}
 
-async function updateRoutineActivity({ id, ...fields }) {}
+async function getRoutineActivitiesByRoutine({ id }) {
+  const { rows: routineActivities } = await client.query(
+    `
+    SELECT * FROM routine_activities
+    WHERE "routineId" = $1;
+    `,
+    [id]
+  );
 
-async function destroyRoutineActivity(id) {}
+  return routineActivities;
+}
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+async function updateRoutineActivity({ id, ...fields }) {
+  const setString = Object.keys(fields)
+    .map((key, idx) => `${key} = $${idx + 1}`)
+    .join(', ');
+
+  const {
+    rows: [updatedRoutineActivity],
+  } = await client.query(
+    `
+  UPDATE routine_activities
+  SET ${setString}
+  WHERE id = ${id}
+  RETURNING *;
+  `,
+    Object.values(fields)
+  );
+
+  return updatedRoutineActivity;
+}
+
+async function destroyRoutineActivity(id) {
+  const {
+    rows: [deletedRoutineActivity],
+  } = await client.query(
+    `
+  DELETE FROM routine_activities
+  WHERE id = $1
+  RETURNING *;
+  `,
+    [id]
+  );
+
+  return deletedRoutineActivity;
+}
+
+async function canEditRoutineActivity(routineActivityId, userId) {
+  const {
+    rows: [routine],
+  } = await client.query(
+    `
+  SELECT * FROM routines
+  WHERE id = (SELECT "routineId" FROM routine_activities WHERE id = $1);
+  `,
+    [routineActivityId]
+  );
+
+  if (routine.creatorId === userId) {
+    return true;
+  } else return false;
+}
 
 module.exports = {
   getRoutineActivityById,
--- db/routines.js
+++ db/routines.js
@@ -19,7 +19,19 @@ async function createRoutine({ creatorId, isPublic, name, goal }) {
   return newRoutine;
 }
 
-async function getRoutineById(id) {}
+async function getRoutineById(id) {
+  const {
+    rows: [routine],
+  } = await client.query(
+    `
+  SELECT * FROM routines
+  WHERE id = $1
+  `,
+    [id]
+  );
+
+  return routine;
+}
 
 async function getRoutinesWithoutActivities() {
   const { rows: routinesWithoutActivities } = await client.query(`
